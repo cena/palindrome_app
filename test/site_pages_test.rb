34627006a9663afd4160ac1896390a1e3298cb28
Cena Mayo <cenazoic@gmail.com>
2021-09-12 09:24:29 -0500

    Added Sinatra layout

--- test/site_pages_test.rb
+++ test/site_pages_test.rb
@@ -10,18 +10,24 @@ class PalindromeAppTest < Minitest::Test
   def test_index
     get '/'
     assert last_response.ok?
+    title_content = doc(last_response).at_css('title').content
     assert doc(last_response).at_css('h1')
+    assert_equal "Palindrome App | Home", title_content
   end
 
   def test_about
     get '/about'
     assert last_response.ok?
+    title_content = doc(last_response).at_css('title').content
     assert doc(last_response).at_css('h1')
+    assert_equal "Palindrome App | About", title_content
   end
 
   def test_palindrome
     get '/palindrome'
     assert last_response.ok?
+    title_content = doc(last_response).at_css('title').content
     assert doc(last_response).at_css('h1')
+    assert_equal "Palindrome App | Palindrome", title_content
   end
 end
